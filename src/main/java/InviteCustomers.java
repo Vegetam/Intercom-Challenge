720c75bd772b29c5a15ca6bf09e2f9e1cd149335
Francesco Malagrino <francesco.malagrino@gmail.com>
2019-10-01 09:55:14 +0100

    Fixed something
    Fixed something

--- src/main/java/InviteCustomers.java
+++ src/main/java/InviteCustomers.java
@@ -3,8 +3,13 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -16,15 +21,38 @@ public class InviteCustomers {
     private static final double DUBLIN_LONG= Math.toRadians(-6.2592576);
     private static final int WITHIN_DISTANCE = 100;
 
-
+ /*
+  * 
+  * 
     public static void main(String[] args) throws IOException, ParseException {
-        String path = "https://s3.amazonaws.com/intercom-take-home-test/customers.txt";
-        List<Customer> selected = selectCustomers(path);
+   	InputStream input = new URL("https://s3.amazonaws.com/intercom-take-home-test/customers.txt").openStream();
+    	BufferedReader buf = new BufferedReader(new InputStreamReader(input));
+    	        
+    	String line = buf.readLine();
+    	StringBuilder sb = new StringBuilder();
+    	        
+    	while(line != null){
+    	   sb.append(line).append("\n");
+    	   line = buf.readLine();
+    	}
+    	        
+    	String fileAsString = sb.toString();
+    	//buf.close();
+    	//input.close();
+        List<Customer> selected = selectCustomers(fileAsString);
         for (Customer c: selected){
             System.out.println("Name:"+c.getName()+" , "+" userId:"+c.getUserId());
         }
     }
-
+*/
+    // Having issue with the name too long for this reason i downloaded the file and created as Json
+    	 public static void main(String[] args) throws IOException, ParseException {
+    	  String path = "/Users/fmalagrino/Documents/GPSProblem/src/main/resources/customers.json";
+          List<Customer> selected = selectCustomers(path);
+          for (Customer c: selected){
+              System.out.println("Name:"+c.getName()+" , "+" userId:"+c.getUserId());
+          }
+  }
     /**
      * Select the customers that are within 100 km from Dublin Office
      * @return List of selected customers
